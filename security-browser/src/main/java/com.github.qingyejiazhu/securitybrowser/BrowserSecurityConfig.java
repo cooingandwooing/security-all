5e11efbe52f96860446003b7492d878c895e1ee5
qingyejiazhu <qingyejiazhu@163.com>
2019-06-24 18:06:12 +0800

    2

--- security-browser/src/main/java/com.github.qingyejiazhu/securitybrowser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com.github.qingyejiazhu/securitybrowser/BrowserSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.github.qingyejiazhu.securitybrowser;
 
+import com.github.qingyejiazhu.securitybrowser.authentication.MyAuthenticationFailureHandler;
 import com.github.qingyejiazhu.securitybrowser.authentication.MyAuthenticationSuccessHandler;
 import com.github.qingyejiazhu.securitycore.properties.SecurityProperties;
+import com.github.qingyejiazhu.securitycore.validate.code.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author gaoxiaofeng
@@ -28,11 +31,17 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+    @Autowired
+    private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.formLogin()
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        // 貌似自动注入了 validateCodeFilter.setFailureHandler(myAuthenticationFailureHandler);
+        http
+                // 由源码得知，在最前面的是UsernamePasswordAuthenticationFilter
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()
                 // 定义表单登录 - 身份认证的方式
                 //.loginPage("/imooc-signIn.html")
                 .loginPage("/authentication/require")
@@ -43,7 +52,7 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 // 别忘记了拦截放行的地方也需要更改为配置类的属性
-                .antMatchers("/authentication/require", securityProperties.getBrowser().getLoginPage()).permitAll()
+                .antMatchers("/authentication/require", securityProperties.getBrowser().getLoginPage(),"/code/image").permitAll()
 //                .antMatchers("/imooc-signIn.html", "/authentication/require").permitAll()
                 .anyRequest()
                 .authenticated()
